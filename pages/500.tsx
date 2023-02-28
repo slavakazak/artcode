98495f2d5962c0673cc33102c13aeee87c56d9ab
slavakazak <slavakazak3@gmail.com>
2023-02-28 21:14:01 +0300

    SEO

--- pages/500.tsx
+++ pages/500.tsx
@@ -2,16 +2,16 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { MainLayout } from '../components/MainLayout'
 
-export default function ErrorPage404() {
+export default function ErrorPage500() {
 	return (
-		<MainLayout title='Ошибка 404'>
-			<section id='page_404'>
+		<MainLayout title='Ошибка 500'>
+			<section id='page_500'>
 				<div className='container'>
 					<div className='row'>
 						<div className='col'>
 							<h1>Упс...</h1>
-							<p className='text'>Похоже мы не можем найти нужную вам страницу</p>
-							<p className='code'>Код ошибки: 404</p>
+							<p className='text'>Что-то пошло не так</p>
+							<p className='code'>Код ошибки: 500</p>
 							<Link href='/'>Вернуться на главную</Link>
 						</div>
 						<div className='col'>

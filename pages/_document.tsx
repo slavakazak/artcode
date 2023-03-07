8330fb730908779f20c0fb9bc896b9cd8e59b5a3
slavakazak <slavakazak3@gmail.com>
2023-03-07 19:31:48 +0300

    Analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
 	return (
@@ -16,6 +17,64 @@ export default function Document() {
 			<body>
 				<Main />
 				<NextScript />
+				{/* <script
+					dangerouslySetInnerHTML={{
+						__html: `
+							(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+							m[i].l=1*new Date();
+							for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+							k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+							(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+							ym(85975805, "init", {
+										clickmap:true,
+										trackLinks:true,
+										accurateTrackBounce:true,
+										webvisor:true
+							});
+						`,
+					}}
+				/> */}
+				<Script id='yandex-metrika' strategy='afterInteractive'>
+					{`
+          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+					m[i].l=1*new Date();
+					for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+					k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+					(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+					ym(${process.env.NEXT_PUBLIC_YANDEX}, "init", {
+						clickmap:true,
+						trackLinks:true,
+						accurateTrackBounce:true,
+						webvisor:true
+					});
+        `}
+				</Script>
+				<noscript>
+					<div>
+						{/* eslint-disable-next-line @next/next/no-img-element */}
+						<img
+							src={`https://mc.yandex.ru/watch/${process.env.NEXT_PUBLIC_YANDEX}`}
+							style={{ position: 'absolute', left: '-9999px' }}
+							alt=''
+						/>
+					</div>
+				</noscript>
+
+				<Script
+					src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE}`}
+					strategy='afterInteractive'
+				/>
+				<Script id='google-analytics' strategy='afterInteractive'>
+					{`
+          window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+
+					gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE}');
+        `}
+				</Script>
 			</body>
 		</Html>
 	)
